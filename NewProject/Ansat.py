a82cc11108e7e4b087db389d9f34199447389424
Peter Sønderstgaard Clausen <1084893@ucn.dk>
2023-05-22 22:43:07 +0200

    Opdatere ansat er næsten færdig og virker med databasen

--- NewProject/Ansat.py
+++ NewProject/Ansat.py
@@ -9,8 +9,8 @@ class Ansat():
         cursor = conn.cursor()
         id = 1
 
-        #Checking for next unused ID
-        CheckForLastUsedId=(
+        # Checking for next unused ID
+        CheckForLastUsedId = (
             "SELECT id FROM employees"
         )
         try:
@@ -19,7 +19,6 @@ class Ansat():
 
             for row in result:
                 id = id + 1
-                #print(row) - test
         except Exception as e:
             print(e)
 
@@ -27,37 +26,40 @@ class Ansat():
         adress = input("Intast adressen på den nye bruger: ")
         phoneNr = input("Indtast telefon nummeret på den nye bruger: ")
 
-        #Inserting bit
-        insertNewUser =(
+        # Inserting bit
+        insertNewUser = (
             "INSERT INTO employees(id, name, adress, phonenr)"
             "VALUES (%s,%s,%s,%s)"
         )
-        data = [id,name,adress,phoneNr]
+        data = [id, name, adress, phoneNr]
         try:
-            cursor.execute(insertNewUser,data)
+            cursor.execute(insertNewUser, data)
             conn.commit()
             print("Data Inserted")
         except Exception as Error:
             print(Error)
         conn.close()
 
-
     def UpdateUser(self):
         conn = connect(host='127.0.0.1', user='root', database='employee', password='meep')
         cursor = conn.cursor()
-
-        #This stores all the IDs
+        IdListOfTuples = []
         IdList = []
-        ListOfIds =("SELECT id FROM employees")
+
+        # This stores all the IDs
+        ListOfIds = ("SELECT id FROM employees")
         try:
             cursor.execute(ListOfIds)
-            IdList = cursor.fetchall()
+            IdListOfTuples = cursor.fetchall()
         except Exception as e:
             print(e)
 
+        for element in IdListOfTuples:
+            intElement = int(element[0])
+            IdList.append(intElement)
 
-        #Prints all useres with ID and Name
-        PrintAllUseres= (
+        # Prints all useres with ID and Name
+        PrintAllUseres = (
             "SELECT id, name FROM employees"
         )
         try:
@@ -68,16 +70,45 @@ class Ansat():
             print(e)
 
         print("\n")
-        userThatNeedsUpdateId = input("Vælge venligest den bruger som du vil opdatere ved at indtaste ID: ")
-        for x in IdList:
-            if x == userThatNeedsUpdateId:
+        userThatNeedsUpdateId = int(input("Vælge venligest den bruger som du vil opdatere ved at indtaste ID: "))
+
+        for item in IdList:
+            if item == userThatNeedsUpdateId:
                 # This inserts the String into a list so that it can be used in the SQL statement
                 data = [userThatNeedsUpdateId]
+
+                # ToString
+                for x in data:
+                    dataToString = str(x)
+
                 SelectUserForUpdate = ("SELECT * FROM employees WHERE id = %s")
                 try:
                     cursor.execute(SelectUserForUpdate, data)
-                    print("yay")
+                    userInfo = cursor.fetchall()
+                    print(tabulate(userInfo, headers=["Navn", "Adresse", "Tlf"]))
+                    print("Hvad vil du opdatere? ")
+                    whatNeedsUpdating = input("Opdatere navn tryk: 1\n"
+                                              "Opdatere adresse tryk: 2\n"
+                                              "Opdatere telefon nummer tryk: 3\n")
+                    if whatNeedsUpdating == "1":
+                        newName = input("Intast det nye navn: ")
+                        updateNameQuery = (
+                            f"update employee.employees Set name = '{newName}' Where (`id` = '{dataToString}');"
+                        )
+                        try:
+                            cursor.execute(updateNameQuery)
+                            conn.commit()
+                        except Exception as e:
+                            print(e)
+                    elif whatNeedsUpdating == "2":
+                        newAdresse = input("Intast den nye adresse: ")
+                        updateAdressQuery = (
+                            f"update employee.employees Set adress = '{newAdresse}' Where (`id` = '{dataToString}');"
+                        )
+                        try:
+                            cursor.execute(updateAdressQuery)
+                            conn.commit()
+                        except Exception as e:
+                            print(e)
                 except Exception as e:
                     print(e)
-            else:
-                print("NO")
